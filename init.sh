a04fcbe206e92913797f2eee68ac28f7a13c3743
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-16 00:04:50 -0700

    Add brew casks

--- init.sh
+++ init.sh
@@ -34,3 +34,41 @@ for tool in "${brew_libs[@]}"; do
     brew install "$tool"
 done
 
+# Install Brew Casks
+brew_casks=(
+  "1password-cli"
+  "1password"
+  "arc"
+  "chatgpt"
+  "cleanshot"
+  "cursor"
+  "discord"
+  "docker"
+  "figma"
+  "github"
+  "google-chrome"
+  "granola"
+  "imageoptim"
+  "localcan"
+  "nordvpn"
+  "notion"
+  "ollama"
+  "pitch"
+  "readdle-spark"
+  "screen-studio"
+  "signal"
+  "slack"
+  "spotify"
+  "typefully"
+  "visual-studio-code"
+  "warp"
+  "whatsapp"
+  "windsurf"
+  "zoom"
+)
+
+# Install each cask
+for cask in "${brew_casks[@]}"; do
+    brew install --cask "$cask"
+done
+
