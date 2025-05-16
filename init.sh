9cea0c90d34bc2b10a692ae6ed00b917e8ab7a78
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-16 00:04:43 -0700

    Add brew libs

--- init.sh
+++ init.sh
@@ -16,3 +16,21 @@ brew upgrade
 
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
+
+# Install Brew libs
+brew_libs=(
+  "btop"
+  "ffmpeg"
+  "git-lfs"
+  "git"
+  "gource"
+  "mas"
+  "stripe"
+  "supabase"
+)
+
+# Install each tool
+for tool in "${brew_libs[@]}"; do
+    brew install "$tool"
+done
+
