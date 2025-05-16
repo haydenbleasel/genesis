52932d4094b39102e54b35fb1e7e07e433e2d983
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-16 00:04:59 -0700

    Add App Store apps

--- init.sh
+++ init.sh
@@ -72,3 +72,19 @@ for cask in "${brew_casks[@]}"; do
     brew install --cask "$cask"
 done
 
+# Create a map of app names to their Mac App Store IDs
+declare -A mas_apps=(
+  ["DaisyDisk"]="411643860"
+  ["Final Cut Pro"]="424389933"
+  ["Flighty"]="1358823008"
+  ["Hand Mirror"]="1502839586"
+  ["Klack"]="6446206067"
+  ["MyMind"]="1520332347"
+  ["Shazam"]="284993459"
+  ["Sidus Link"]="1471951977"
+)
+
+# Install each app from Mac App Store
+for app_name in "${!mas_apps[@]}"; do
+    mas install "${mas_apps[$app_name]}"
+done
