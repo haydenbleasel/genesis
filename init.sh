c48e46c6a844c1d793592db1addd49b5c501c0d6
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-16 00:04:33 -0700

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Check if Homebrew is installed
+if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
