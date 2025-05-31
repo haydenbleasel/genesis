a607e7dd0b0ae47eb45c7fe0d33c6ceb5d4d3614
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-30 23:22:27 -0700

    Update mas.sh

--- lib/mas.sh
+++ lib/mas.sh
@@ -1,14 +1,13 @@
-declare -A mas_apps=(
-  ["DaisyDisk"]="411643860"
-  ["Final Cut Pro"]="424389933"
-  ["Flighty"]="1358823008"
-  ["Hand Mirror"]="1502839586"
-  ["Klack"]="6446206067"
-  ["MyMind"]="1520332347"
-  ["Shazam"]="284993459"
-  ["Sidus Link"]="1471951977"
+mas_apps=(
+  "411643860" # DaisyDisk
+  "424389933" # FinalCutPro
+  "1358823008" # Flighty
+  "1502839586" # HandMirror
+  "6446206067" # Klack
+  "1532801185" # MyMind
+  "897118787" # Shazam
 )
 
-for app_name in "${!mas_apps[@]}"; do
-    mas install "${mas_apps[$app_name]}"
+for app_id in "${mas_apps[@]}"; do
+    mas install "$app_id"
 done
